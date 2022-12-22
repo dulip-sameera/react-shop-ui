c37b75a9eb41a32a11bd4e669c6bc95ca5737ae9
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-22 18:04:08 +0530

    add navigation links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,30 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <nav className="d-flex justify-content-center">
+            <div className="nav nav-pills">
+              <Link to="/" className="">
+                Home
+              </Link>
+              <Link to="about" className="">
+                About
+              </Link>
+              <Link to="products" className="">
+                Products
+              </Link>
+              <Link to="posts" className="">
+                Posts
+              </Link>
+            </div>
+          </nav>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,36 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <header>
+        <div className="container">
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="logo">
+              <Link to="/" className="brand">
+                WSP
+              </Link>
+            </div>
+
+            <div className="nav nav-pills">
+              <Link to="/" className="">
+                Home
+              </Link>
+              <Link to="about" className="">
+                About
+              </Link>
+              <Link to="products" className="">
+                Products
+              </Link>
+              <Link to="posts" className="">
+                Posts
+              </Link>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
