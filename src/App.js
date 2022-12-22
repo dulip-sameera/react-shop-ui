7039519940576a9d860a81315f07ee0db971f46c
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-22 17:46:05 +0530

    setup routers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header, Footer } from "./components";
+import { Home, About, Products, SingleProduct, Posts, Error } from "./pages";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div>Heeelllo</div>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="products" element={<Products />} />
+          <Route path="products/:productId" element={<SingleProduct />} />
+          <Route path="posts" element={<Posts />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
