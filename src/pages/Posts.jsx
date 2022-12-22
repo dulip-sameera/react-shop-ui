d0055b42006a4f6ae060ef0f5380472175035580
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-22 23:19:19 +0530

    add posts content

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Posts = () => {
-  return <div>Posts</div>;
+  const [posts, setPosts] = useState([]);
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    await fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setPosts(json))
+      .then(setLoader(false));
+  };
+
+  return (
+    <main className="posts">
+      <div className="pg-header">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-7">
+              <h1>Products</h1>
+            </div>
+            <div className="col-lg-5">
+              <nav>
+                <ol className="breadcrumb justify-content-end">
+                  <li className="breadcrumb-item">
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li className="breadcrumb-item active">Posts</li>
+                </ol>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container content">
+        {loader ? (
+          <div className="text-center">Loading...</div>
+        ) : (
+          <div className="row">
+            {posts.map((post) => {
+              return (
+                <div className="col-sm-6" key={post.id}>
+                  <div className="card">
+                    <div className="card-body">
+                      <h5 className="card-title">{post.title}</h5>
+                      <p className="card-text">{post.body}</p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </main>
+  );
 };
 
 export default Posts;
