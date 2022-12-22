4f790034aa7f5d61e719778973c9e73f7eccfb9b
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-22 17:46:34 +0530

    create all the required component

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Home from "./Home";
+import About from "./About";
+import Products from "./Products";
+import SingleProduct from "./SingleProduct";
+import Posts from "./Posts";
+import Error from "./Error";
+
+export { Home, About, Products, SingleProduct, Posts, Error };
