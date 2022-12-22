aa73a450b7ca0bf2b3f77720a901176d2309adc4
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-22 17:48:33 +0530

    import bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header, Footer } from "./components";
 import { Home, About, Products, SingleProduct, Posts, Error } from "./pages";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
