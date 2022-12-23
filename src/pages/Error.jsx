87872fbaec829577dfb417d405beffd2e3282743
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-23 07:02:13 +0530

    add content

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Error = () => {
-  return <div>Error</div>;
+  const navigate = useNavigate();
+  return (
+    <main>
+      <div className="pg-header">
+        <div className="container">
+          <h1>404: NOT FOUND</h1>
+        </div>
+      </div>
+      <div className="container content">
+        <p>Page not found...</p>
+        <Link to={"/"} className="btn btn-primary" onClick={() => navigate(-1)}>
+          Go Back
+        </Link>
+      </div>
+    </main>
+  );
 };
 
 export default Error;
