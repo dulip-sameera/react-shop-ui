b5b555b64c421a4e941e2d944841fc8c86b751a1
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-23 07:08:40 +0530

    add content

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import { Header, Footer } from "../components";
 
 const MainLayout = () => {
-  return <div>MainLayout</div>;
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
 };
 
 export default MainLayout;
